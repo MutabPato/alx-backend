692c4eeebeca866ea64af21d8ce182e2a44952a5
MutabPato <patrickmtbz@gmail.com>
2024-07-30 12:00:50 -0700

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -30,7 +30,7 @@ def get_locale():
     """
     determine the best match with our supported languages.
     """
-    return request.accept_languages.best_matches(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
