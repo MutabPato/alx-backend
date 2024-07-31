5f35f57eea27ff8c5a45e46a3e00d5030e92f120
MutabPato <patrickmtbz@gmail.com>
2024-07-31 00:12:12 -0700

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-""" i18n - Basic Flask App """
+""" i18n - Basic Flask App
+Functions:
+    index: Renders the index page.
+Usage:
+    To run the application, execute the script with a Python interpreter.
+
+Example:
+    $ ./0-app.py
+"""
 
 from flask import Flask, render_template
 from flask_babel import Babel, gettext as _
