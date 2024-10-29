484a63ba33918f4377beaab3f4f6ff4342f0a899
MutabPato <patrickmtbz@gmail.com>
2024-10-29 08:53:34 -0700

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,6 @@ class BasicCache(BaseCaching):
     """BasicCache inherits from BaseCaching and is a caching system
     - implements put and get methods
     """
-    def __init__(self):
-        """Initialize
-        """
-        super().__init__()
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data
@@ -24,7 +20,5 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """return the value in self.cache_data linked to key
         """
-        if key is None or self.cache_data.get(key) is None:
-            return None
 
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
