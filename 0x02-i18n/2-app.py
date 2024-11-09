09d1c6d3051de6b1a2e4a05c346f0331402ccc2a
MutabPato <patrickmtbz@gmail.com>
2024-11-09 06:57:04 -0800

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+i18n
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configures babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
