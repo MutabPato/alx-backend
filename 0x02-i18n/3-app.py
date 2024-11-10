7243c7bc331682023fc07d531e10ee609c3311b2
MutabPato <patrickmtbz@gmail.com>
2024-11-10 00:07:23 -0800

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,18 +19,19 @@ class Config:
 
 
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Get locale from request
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Index
     """
     return render_template("3-index.html")
